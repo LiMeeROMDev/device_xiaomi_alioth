44e6fce34e13c7efb892685a0a9a7f8c7965a19a
LiMee233 <LiMee233@outlook.com>
2022-07-01 18:31:39 +0800

    alioth: Disable TARGET_ENABLE_AUDIO_ULL * Causes the device to not play sound. * Partially reverted commit 3cb8cca053c3143d93774197f89db251d2d2c14d * From https://github.com/xiaomi-sm8250-devs/android_device_xiaomi_alioth/commit/cd8e2be2a9f2fad9bd1204081aeb6abb70a88405

--- device.mk
+++ device.mk
@@ -93,8 +93,6 @@ PRODUCT_PACKAGES += \
     otapreopt_script
 
 # Audio
-TARGET_ENABLE_AUDIO_ULL := true
-
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
