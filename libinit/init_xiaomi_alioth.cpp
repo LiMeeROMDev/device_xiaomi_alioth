f60cb69c8f7c430e3bde1ad464ec329640d8172d
LiMee233 <LiMee233@outlook.com>
2022-07-01 14:15:50 +0800

    alioth: Refactor libinit
    Co-authored-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- libinit/init_xiaomi_alioth.cpp
+++ libinit/init_xiaomi_alioth.cpp
@@ -1,57 +1,96 @@
 /*
- * Copyright (C) 2021 The LineageOS Project
+ * Copyright (C) 2021 Paranoid Android
  *
- * SPDX-License-Identifier: Apache-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+#include <vector>
 
-#include <libinit_dalvik_heap.h>
-#include <libinit_variant.h>
+#include <android-base/properties.h>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
+#include <sys/sysinfo.h>
 
-#include "vendor_init.h"
+using android::base::GetProperty;
 
-static const variant_info_t aliothin_info = {
-    .hwc_value = "INDIA",
-    .sku_value = "",
+void property_override(char const prop[], char const value[], bool add = true) {
+    prop_info* pi;
 
-    .brand = "Xiaomi",
-    .device = "aliothin",
-    .marketname = "Mi 11X",
-    .model = "M2012K11AI",
+    pi = (prop_info*)__system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else if (add)
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
 
-    .nfc = false,
-};
+void load_dalvikvm_properties() {
+    struct sysinfo sys;
+    sysinfo(&sys);
+    if(sys.totalram > 6144ull * 1024 * 1024) {
+    // from - phone-xhdpi-8192-dalvik-heap.mk
+        property_override("dalvik.vm.heapstartsize", "24m");
+        property_override("dalvik.vm.heaptargetutilization", "0.46");
+        property_override("dalvik.vm.heapmaxfree", "48m");
+    }
 
-static const variant_info_t alioth_global_info = {
-    .hwc_value = "GLOBAL",
-    .sku_value = "",
+    else {
+    // from - phone-xhdpi-6144-dalvik-heap.mk
+        property_override("dalvik.vm.heapstartsize", "16m");
+        property_override("dalvik.vm.heaptargetutilization", "0.5");
+        property_override("dalvik.vm.heapmaxfree", "32m");
+    }
+    property_override("dalvik.vm.heapgrowthlimit", "256m");
+    property_override("dalvik.vm.heapsize", "512m");
+    property_override("dalvik.vm.heapminfree", "8m");
+}
 
-    .brand = "POCO",
-    .device = "alioth",
-    .marketname = "POCO F3",
-    .model = "M2012K11AG",
+void load_redmi_k40() {
+    property_override("ro.boot.product.hardware.sku", "nfc");
+    property_override("ro.product.model", "M2012K11AC");
+    property_override("ro.product.brand", "Redmi");
+    property_override("ro.product.vendor.marketname", "Redmi K40");
+    property_override("ro.product.vendor.manufacturer", "Xiaomi");
+    property_override("ro.product.vendor.brand", "Redmi");
+    property_override("ro.product.vendor.model", "M2012K11AC");
+}
 
-    .nfc = true,
-};
+void load_poco_f3() {
+    property_override("ro.boot.product.hardware.sku", "nfc");
+    property_override("ro.product.model", "M2012K11AG");
+    property_override("ro.product.brand", "POCO");
+    property_override("ro.product.vendor.marketname", "POCO F3");
+    property_override("ro.product.vendor.manufacturer", "Xiaomi");
+    property_override("ro.product.vendor.brand", "POCO");
+    property_override("ro.product.vendor.model", "M2012K11AG");
+}
 
-static const variant_info_t alioth_info = {
-    .hwc_value = "",
-    .sku_value = "",
-
-    .brand = "Redmi",
-    .device = "alioth",
-    .marketname = "K40",
-    .model = "M2012K11AC",
-
-    .nfc = true,
-};
-
-static const std::vector<variant_info_t> variants = {
-    aliothin_info,
-    alioth_global_info,
-    alioth_info,
-};
+void load_xiaomi_mi11x() {
+    property_override("ro.product.model", "M2012K11AI");
+    property_override("ro.product.brand", "Mi");
+    property_override("ro.product.vendor.marketname", "Mi 11X");
+    property_override("ro.product.vendor.manufacturer", "Xiaomi");
+    property_override("ro.product.vendor.brand", "Mi");
+    property_override("ro.product.vendor.model", "M2012K11AI");
+}
 
 void vendor_load_properties() {
-    search_variant(variants);
-    set_dalvik_heap();
+    std::string region = GetProperty("ro.boot.hwc", "");
+    if (region.find("CN") != std::string::npos) {
+        load_redmi_k40();
+    } else if (region.find("INDIA") != std::string::npos) {
+        load_xiaomi_mi11x();
+    } else {
+        load_poco_f3();
+    }
+    
+    load_dalvikvm_properties();
 }
