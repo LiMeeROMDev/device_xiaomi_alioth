c2b8ba38eda68b4b43a0fff08d65690ec2a9e961
LiMee233 <LiMee233@outlook.com>
2022-08-08 21:01:08 +0800

    alioth: Drop custom vibrator * Already in source, useless.

--- device.mk
+++ device.mk
@@ -449,10 +449,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service.xiaomi_kona
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+    vendor.qti.hardware.vibrator.service
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
