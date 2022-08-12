803d55f6027b6331034ba14b538d4db7289c2e4b
LiMee233 <LiMee233@outlook.com>
2022-08-12 08:00:50 +0800

    Move to perf kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -100,14 +100,16 @@ TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_alioth
 BOARD_BOOT_HEADER_VERSION := 3
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 loop.max_part=7 cgroup.memory=nokmem,nosocket reboot=panic_warm
+BOARD_KERNEL_CMDLINE += androidboot.fstab_suffix=qcom
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
-TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8250
 TARGET_KERNEL_CONFIG := alioth_defconfig
+TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8250
+
 
 # OTA assert
 TARGET_OTA_ASSERT_DEVICE := alioth,aliothin
