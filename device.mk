02c4217872746a7e070f697497a549b0ab238e54
LiMee233 <LiMee233@outlook.com>
2022-11-08 15:03:54 +0800

    Build libpiex_shim * Also include it in vendor.

--- device.mk
+++ device.mk
@@ -147,6 +147,7 @@ PRODUCT_PACKAGES += \
     libcamera2ndk_vendor \
     libgui_vendor \
     libstdc++.vendor \
+    libpiex_shim \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
